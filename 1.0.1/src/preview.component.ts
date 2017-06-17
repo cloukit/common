ee012af981a6cdbf5d387d9c7c0c5d038b1c8d73
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-06-17 20:21:34 +0200

    doc

--- 1.0.1/src/preview.component.ts
+++ 1.0.1/src/preview.component.ts
@@ -17,6 +17,16 @@ import { Component } from '@angular/core';
       name="iAmSomeWhereElse"
       [cloukitHasFocus]="hasFocus"
     />
+
+
+    <h3>ClickOutside</h3>
+    <input
+      type="text"
+      value="click inside me and outside to see the change"
+      [ngStyle]="containerStyles()"
+      (click)="toggleFocus2(true)"
+      (cloukitClickOutside)="toggleFocus2(false)"
+    />
   </div>`,
   styles: [ '.preview { font-family:sans-serif' ],
 })
@@ -24,9 +34,34 @@ export class PreviewComponent {
 
   constructor() { }
 
+  // HAS FOCUS
   hasFocus = false;
 
   toggleFocus(value: boolean) {
     this.hasFocus = !this.hasFocus;
+  }
+
+  // CLICK OUTSIDE
+  hasFocus2 = false;
+  styles = {
+    inActive: {
+      backgroundColor: '#FAFBFC',
+      borderColor: '#F4F5F7',
+      width: '300px',
+    },
+    active: {
+      backgroundColor: '#FFFFFF',
+      borderColor: '#4C9AFF',
+      width: '300px',
+      transition: 'background-color 0.2s ease-in-out, border-color 0.2s ease-in-out',
+    }
+  };
+
+  containerStyles() {
+    return this.hasFocus2 ? this.styles.active : this.styles.inActive;
+  }
+
+  toggleFocus2(value: boolean) {
+    this.hasFocus2 = !this.hasFocus2;
   }
 }
