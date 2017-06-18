595d95eaab8d6031a7e86fe7f3cacb62a81966cd
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-06-18 15:43:00 +0200

    v1.0.2 and jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git clone https://github.com/cloukit/ng-library-build.git library-build-chain
+git clone https://github.com/cloukit/library-build-chain.git library-build-chain
 cd library-build-chain
 npm install
 npm run build
