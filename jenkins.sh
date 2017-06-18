54ad3292e552482bc3e002009e357d3fcf8d6231
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-06-18 15:59:02 +0200

    trigger build

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,8 @@
 # BUILD TRIGGERED BY: https://github.com/codeclou/jenkins-github-webhook-build-trigger-plugin
 #
 
+set -e
+
 git clone https://github.com/cloukit/library-build-chain.git library-build-chain
 cd library-build-chain
 bash jenkins.sh
