ec4d2d2ffc2c4305906f40e89365d2159459eb6b
Bernhard Grünewaldt <bitbucket@gruenewaldt.net>
2017-06-25 20:02:33 +0200

    v1.1.0

--- 1.0.2/src/main.ts
+++
@@ -1,7 +0,0 @@
-//our root app component
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteParams, RouteConfig, Router, LocationStrategy, HashLocationStrategy } from '@angular2/router';
-import { enableProdMode } from '@angular/core';
-import { AppModule }  from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
