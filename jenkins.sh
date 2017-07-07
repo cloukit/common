4665e971798bd5ef4ab0db59c11f7afdd4d9a8aa
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-07-07 21:03:04 +0200

    1.4.0 new npm based build chain

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-#
 # BUILD TRIGGERED BY: https://github.com/codeclou/jenkins-github-webhook-build-trigger-plugin
-#
-
 set -e
-
-git clone https://github.com/cloukit/library-build-chain.git library-build-chain
-cd library-build-chain
+git clone https://github.com/cloukit/library-deploy-chain.git library-deploy-chain
+cd library-deploy-chain
 bash jenkins.sh
