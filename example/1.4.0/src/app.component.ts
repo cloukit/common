e2d72245146fae42bf42249105ab84b5da984840
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-07-07 21:45:33 +0200

    1.4.0

---
+++ example/1.4.0/src/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div>
+    <preview></preview>
+  </div>`,
+  styles: [  ],
+})
+export class AppComponent {
+
+}
