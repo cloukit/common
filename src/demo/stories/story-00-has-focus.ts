db5d83e59b21e31b1ade0efcd4e402e3944551b6
clouless <github@gruenewaldt.net>
2018-03-28 17:23:12 +0200

    1.6.2 latest build chain. stories for doc.

---
+++ src/demo/stories/story-00-has-focus.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { DemoComponent } from '../demo.component';
+
+@Component({
+  selector: 'story-00-has-focus',
+  templateUrl: './story-00-has-focus.html',
+  styles: [ ].concat(DemoComponent.sharedStyles),
+})
+export class Story00Component {
+
+  styles: any = { };
+
+  hasFocus = false;
+
+  constructor() {
+    this.styles.base = {
+      color: '#000',
+      borderWidth: '3px',
+      borderStyle: 'solid',
+      width: '300px',
+      padding: '8px',
+    };
+    this.styles.inActive = Object.assign({}, this.styles.base, {
+      backgroundColor: '#FAFBFC',
+      borderColor: '#F4F5F7',
+    });
+    this.styles.active = Object.assign({}, this.styles.base, {
+      color: '#fff',
+      backgroundColor: '#64B058',
+      borderColor: '#000',
+      transition: [
+        'color 0.2s ease-in-out, ',
+        'background-color 0.2s ease-in-out, ',
+        'border-color 0.2s ease-in-out '
+      ].join(''),
+    });
+  }
+
+  focusStyles() {
+    return this.hasFocus ? this.styles.active : this.styles.inActive;
+  }
+
+  toggleFocus() {
+    this.hasFocus = !this.hasFocus;
+  }
+
+}
